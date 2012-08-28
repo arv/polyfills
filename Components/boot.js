291fc992f1ca2e49b0de02d695b478e8fe1ad177
Scott Miles <sjmiles@google.com>
2012-08-27 18:19:03 -0700

    whitespace changes only: code style compliance (spaces not tabs, 80 col width)

--- Components/boot.js
+++ Components/boot.js
@@ -5,30 +5,30 @@ scope = scope || {};
 // NOTE: uses 'window' and 'document' globals
 
 scope.ready = function() {
-	/*
-	scope.declarationFactory.createHostSheet();
-	scope.componentLoader.preload(document, function() {
-		scope.parser.parse(document, scope.declarationFactory.declarationFromElement);
-		scope.declarationFactory.morphAll(document);
-	*/
-		scope.webComponentsReady();
-	/*
-	});
-	*/
+  /*
+scope.declarationFactory.createHostSheet();
+scope.componentLoader.preload(document, function() {
+  scope.parser.parse(document, scope.declarationFactory.declarationFromElement);
+  scope.declarationFactory.morphAll(document);
+*/
+  scope.webComponentsReady();
+/*
+});
+*/
 };
 
 scope.webComponentsReady = function() {
-	var e = document.createEvent('Event');
-	e.initEvent('WebComponentsReady', true, true);
-	window.document.body.dispatchEvent(e);
+  var e = document.createEvent('Event');
+  e.initEvent('WebComponentsReady', true, true);
+  window.document.body.dispatchEvent(e);
 };
 
 scope.run = function() {
-	document.addEventListener('DOMContentLoaded', scope.ready);
+  document.addEventListener('DOMContentLoaded', scope.ready);
 };
 
 if (!scope.flags.runManually) {
-	scope.run();
+  scope.run();
 }
 
 // we are using this object for cross-module support only
