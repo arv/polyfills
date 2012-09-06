1e3f3e759febe2f0fc21f977fc9a51cf873c6009
Scott Miles <sjmiles@google.com>
2012-09-06 12:26:20 -0700

    spaces -> tabs, add installDom stub

--- Components/ShadowDom/WebkitShadowDom.js
+++ Components/ShadowDom/WebkitShadowDom.js
@@ -3,16 +3,18 @@
 scope = scope || {};
 
 scope.webkitShadowImpl = {
-    createShadowDom: function(inInstance, inContents) {
-		var shadowRoot = new WebKitShadowRoot(inInstance);
-		// TODO(sjmiles): check spec: .host not set automatically
-		if (!shadowRoot.host) {
-			shadowRoot.host = inInstance;
-		}
-      inInstance.shadow = inContents;
-      shadowRoot.appendChild(inContents);
-      return shadowRoot;
+  createShadowDom: function(inInstance, inContents) {
+    var shadowRoot = new WebKitShadowRoot(inInstance);
+    // TODO(sjmiles): check spec: .host not set automatically
+    if (!shadowRoot.host) {
+      shadowRoot.host = inInstance;
     }
+    inInstance.shadow = inContents;
+    shadowRoot.appendChild(inContents);
+    return shadowRoot;
+  },
+  installDom: function() {
+  }
 };
 
 })(window.__exported_components_polyfill_scope__);
